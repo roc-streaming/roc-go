d0876fc6050644c27c3a7f751ece5689cac5c855
Asalle <asalle.kim@gmail.com>
2020-04-01 08:41:09 +0200

    receiver: copy receiver config field by field

--- roc/receiver.go
+++ roc/receiver.go
@@ -16,9 +16,20 @@ import (
 
 func OpenReceiver(rocContext *Context, receiverConfig *ReceiverConfig) (*Receiver, error) {
 	rocContextCPtr := (*C.roc_context)(unsafe.Pointer(rocContext))
-	receiverConfigCPtr := (*C.roc_receiver_config)(unsafe.Pointer(receiverConfig))
+	receiverConfigC := C.struct_roc_receiver_config{
+		frame_sample_rate:         (C.uint)(receiverConfig.FrameSampleRate),
+		frame_channels:            (C.roc_channel_set)(receiverConfig.FrameChannels),
+		frame_encoding:            (C.roc_frame_encoding)(receiverConfig.FrameEncoding),
+		automatic_timing:          (C.uint)(receiverConfig.AutomaticTiming),
+		resampler_profile:         (C.roc_resampler_profile)(receiverConfig.ResamplerProfile),
+		target_latency:            (C.ulonglong)(receiverConfig.TargetLatency),
+		max_latency_overrun:       (C.ulonglong)(receiverConfig.MaxLatencyOverrun),
+		no_playback_timeout:       (C.longlong)(receiverConfig.NoPlaybackTimeout),
+		broken_playback_timeout:   (C.longlong)(receiverConfig.BrokenPlaybackTimeout),
+		breakage_detection_window: (C.ulonglong)(receiverConfig.BreakageDetectionWindow),
+	}
 
-	receiver := C.roc_receiver_open(rocContextCPtr, receiverConfigCPtr)
+	receiver := C.roc_receiver_open(rocContextCPtr, &receiverConfigC)
 	if receiver == nil {
 		return nil, ErrInvalidArguments
 	}
