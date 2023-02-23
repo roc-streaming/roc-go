ba606e38bfd1a8a0ea40e608421aeabba582ed8f
Victor Gaydov <victor@enise.org>
2023-02-23 17:46:44 +0400

    Don't use pkg-config on mac

---
+++ roc/flags_nix.go
@@ -0,0 +1,9 @@
+//go:build aix || dragonfly || freebsd || hurd || illumos || linux || netbsd || openbsd || solaris
+// +build aix dragonfly freebsd hurd illumos linux netbsd openbsd solaris
+
+package roc
+
+/*
+#cgo pkg-config: roc
+*/
+import "C"
