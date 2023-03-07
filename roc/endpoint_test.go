9279c29564a2f9031dfb713d47ffffb5f2b26257
Victor Gaydov <victor@enise.org>
2023-03-07 18:06:04 +0400

    Minor refactoring in endpoint tests

--- roc/endpoint_test.go
+++ roc/endpoint_test.go
@@ -17,33 +17,7 @@ func TestEndpoint(t *testing.T) {
 		parseErr   error
 		composeErr error
 	}{
-		{
-			uri:        "rtsp://192.168.0.1",
-			protocol:   ProtoRtsp,
-			host:       "192.168.0.1",
-			port:       -1,
-			resource:   "",
-			parseErr:   nil,
-			composeErr: nil,
-		},
-		{
-			uri:        "rtsp://192.168.0.1:12345",
-			protocol:   ProtoRtsp,
-			host:       "192.168.0.1",
-			port:       12345,
-			resource:   "",
-			parseErr:   nil,
-			composeErr: nil,
-		},
-		{
-			uri:        "rtsp://192.168.0.1:12345/path",
-			protocol:   ProtoRtsp,
-			host:       "192.168.0.1",
-			port:       12345,
-			resource:   "/path",
-			parseErr:   nil,
-			composeErr: nil,
-		},
+		// protocols
 		{
 			uri:        "rtsp://192.168.0.1:12345/path?query1=query1&query2=query2",
 			protocol:   ProtoRtsp,
@@ -54,77 +28,12 @@ func TestEndpoint(t *testing.T) {
 			composeErr: nil,
 		},
 		{
-			uri:        "",
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_set_protocol()", -1),
-		},
-		{
-			uri:        "rtsp://",
-			protocol:   ProtoRtsp,
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
-		},
-		{
-			uri:        "rtsp://:12345",
-			protocol:   ProtoRtsp,
-			port:       12345,
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
-		},
-		{
-			uri:        "rtsp://192.168.0.1:65536",
-			protocol:   ProtoRtsp,
-			host:       "192.168.0.1",
-			port:       655356,
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_set_port()", -1),
-		},
-		{
-			uri:        "rtsp://192.168.0.1:-2",
-			protocol:   ProtoRtsp,
-			host:       "192.168.0.1",
-			port:       -2,
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_set_port()", -1),
-		},
-		{
-			uri:        "rtsp://192.168.0.1/??",
-			protocol:   ProtoRtsp,
-			host:       "192.168.0.1",
-			port:       -1,
-			resource:   "??",
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_set_resource()", -1),
-		},
-		{
-			protocol:   Protocol(1),
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_set_protocol()", -1),
-		},
-		{
 			uri:        "rtp://192.168.0.1:12345",
 			protocol:   ProtoRtp,
 			host:       "192.168.0.1",
 			port:       12345,
 			parseErr:   nil,
 			composeErr: nil,
-		},
-		{
-			uri:        "rtp://192.168.0.1:12345/path",
-			protocol:   ProtoRtp,
-			host:       "192.168.0.1",
-			port:       12345,
-			resource:   "/path",
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
-		},
-		{
-			uri:        "rtp://192.168.0.1",
-			protocol:   ProtoRtp,
-			host:       "192.168.0.1",
-			port:       -1,
-			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
-			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
 		},
 		{
 			uri:        "rtp+rs8m://192.168.0.1:12345",
@@ -166,6 +75,111 @@ func TestEndpoint(t *testing.T) {
 			port:       12345,
 			parseErr:   nil,
 			composeErr: nil,
+		},
+		// components
+		{
+			uri:        "rtsp://192.168.0.1",
+			protocol:   ProtoRtsp,
+			host:       "192.168.0.1",
+			port:       -1, // use default rtsp port
+			resource:   "",
+			parseErr:   nil,
+			composeErr: nil,
+		},
+		{
+			uri:        "rtsp://192.168.0.1:12345",
+			protocol:   ProtoRtsp,
+			host:       "192.168.0.1",
+			port:       12345,
+			resource:   "",
+			parseErr:   nil,
+			composeErr: nil,
+		},
+		{
+			uri:        "rtsp://192.168.0.1:12345/path",
+			protocol:   ProtoRtsp,
+			host:       "192.168.0.1",
+			port:       12345,
+			resource:   "/path",
+			parseErr:   nil,
+			composeErr: nil,
+		},
+		/* FIXME: uncomment after https://github.com/roc-streaming/roc-toolkit/issues/519
+			{
+				uri:        "rtsp://192.168.0.1:0",
+				protocol:   ProtoRtsp,
+				host:       "192.168.0.1",
+				port:       0, // use zero port (for bind)
+				resource:   "",
+				parseErr:   nil,
+				composeErr: nil,
+			},
+		*/
+		// errors
+		{
+			uri:        "", // empty uri
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_set_protocol()", -1),
+		},
+		{
+			uri:        "rtsp://", // missing host and port
+			protocol:   ProtoRtsp,
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
+		},
+		{
+			uri:        "rtsp://:12345", // missing host
+			protocol:   ProtoRtsp,
+			port:       12345,
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
+		},
+		{
+			uri:        "rtsp://192.168.0.1:65536", // port out of range
+			protocol:   ProtoRtsp,
+			host:       "192.168.0.1",
+			port:       655356,
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_set_port()", -1),
+		},
+		{
+			uri:        "rtsp://192.168.0.1:-2", // port out of range
+			protocol:   ProtoRtsp,
+			host:       "192.168.0.1",
+			port:       -2,
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_set_port()", -1),
+		},
+		{
+			uri:        "rtsp://192.168.0.1/??", // invalid resource
+			protocol:   ProtoRtsp,
+			host:       "192.168.0.1",
+			port:       -1,
+			resource:   "??",
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_set_resource()", -1),
+		},
+		{
+			protocol:   Protocol(1), // invalid protocol
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_set_protocol()", -1),
+		},
+		{
+			uri:        "rtp://192.168.0.1:12345/path", // resource not allowed for protocol
+			protocol:   ProtoRtp,
+			host:       "192.168.0.1",
+			port:       12345,
+			resource:   "/path",
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
+		},
+		{
+			uri:        "rtp://192.168.0.1", // default port not defined for protocol
+			protocol:   ProtoRtp,
+			host:       "192.168.0.1",
+			port:       -1,
+			parseErr:   newNativeErr("roc_endpoint_set_uri()", -1),
+			composeErr: newNativeErr("roc_endpoint_get_uri()", -1),
 		},
 	}
 
