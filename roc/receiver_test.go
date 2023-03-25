20fef1546fca3e68cbd7ab884f895e40481ee14e
Darrel Idiagbor <idiagbordarrel@gmail.com>
2023-03-25 16:20:22 +0100

    feat: test receiver read floats implemented

--- roc/receiver_test.go
+++ roc/receiver_test.go
@@ -215,3 +215,77 @@ func TestReceiver_Bind(t *testing.T) {
 		})
 	}
 }
+
+func TestReceiver_ReadFloats(t *testing.T) {
+	baseFrameCnt := 2
+	baseFrame := make([]float32, baseFrameCnt)
+	for i := 0; i < baseFrameCnt; i++ {
+		baseFrame[i] = float32(i + 1)
+	}
+
+	cases := []struct {
+		name                 string
+		frame                []float32
+		receiverClosedBefore bool
+		wantErr              error
+	}{
+		{
+			name:                 "ok",
+			frame:                baseFrame,
+			receiverClosedBefore: false,
+			wantErr:              nil,
+		},
+		{
+			name:                 "closed receiver",
+			frame:                baseFrame,
+			receiverClosedBefore: true,
+			wantErr:              errors.New("receiver is closed"),
+		},
+		{
+			name:                 "nil frame",
+			receiverClosedBefore: false,
+			wantErr:              errors.New("frame is nil"),
+		},
+		{
+			name:                 "bad frame",
+			frame:                []float32{1.0},
+			receiverClosedBefore: false,
+			wantErr:              newNativeErr("roc_receiver_read()", -1),
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, err := OpenContext(ContextConfig{})
+			require.NoError(t, err)
+
+			receiver, err := OpenReceiver(ctx, ReceiverConfig{
+				FrameSampleRate: 44100,
+				FrameChannels:   ChannelSetStereo,
+				FrameEncoding:   FrameEncodingPcmFloat,
+			})
+			require.NoError(t, err)
+			require.NotNil(t, receiver)
+
+			if tt.receiverClosedBefore {
+				err = receiver.Close()
+				require.NoError(t, err)
+			}
+
+			err = receiver.ReadFloats(tt.frame)
+			if tt.wantErr != nil {
+				require.Equal(t, tt.wantErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+			}
+
+			if !tt.receiverClosedBefore {
+				err = receiver.Close()
+				require.NoError(t, err)
+			}
+
+			err = ctx.Close()
+			require.NoError(t, err)
+		})
+	}
+}
