5263374a9064b144ffbbf1bf24389961f871357a
Victor Gaydov <victor@enise.org>
2023-05-16 14:04:57 +0400

    Refactor version checks and improve tests

--- roc/receiver.go
+++ roc/receiver.go
@@ -172,7 +172,7 @@ type Receiver struct {
 // Open a new receiver.
 // Allocates and initializes a new receiver, and attaches it to the context.
 func OpenReceiver(context *Context, config ReceiverConfig) (*Receiver, error) {
-	versionCheckFn()
+	checkVersionFn()
 
 	if context == nil {
 		return nil, errors.New("context is nil")
--- roc/version_test.go
+++ roc/version_test.go
@@ -4,20 +4,135 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestVersion_Info(t *testing.T) {
 	v := Version()
 
-	require.NotZero(t, v.Native.Major+v.Native.Minor+v.Native.Patch)
-	require.NotZero(t, v.Bindings.Major+v.Bindings.Minor+v.Bindings.Patch)
-	require.Equal(t, v.Native.Major, v.Bindings.Major)
-	require.GreaterOrEqual(t, v.Bindings.Minor, v.Native.Minor)
+	assert.NotZero(t, v.Native.Major+v.Native.Minor+v.Native.Patch)
+	assert.NotZero(t, v.Bindings.Major+v.Bindings.Minor+v.Bindings.Patch)
+	assert.Equal(t, v.Native.Major, v.Bindings.Major)
+	assert.GreaterOrEqual(t, v.Bindings.Minor, v.Native.Minor)
 }
 
 func TestVersion_Check(t *testing.T) {
-	require.NotPanics(t, func() { versionCheck() })
+	tests := []struct {
+		name      string
+		version   string
+		wantPanic bool
+	}{
+		{
+			name:      "ok",
+			version:   bindingsVersion,
+			wantPanic: false,
+		},
+		{
+			name:      "validation error",
+			version:   "999.999.999",
+			wantPanic: true,
+		},
+		{
+			name:      "parsing error",
+			version:   "xxx",
+			wantPanic: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			originalBindingsVersion := bindingsVersion
+			defer func() { bindingsVersion = originalBindingsVersion }()
+
+			bindingsVersion = tt.version
+			versionCheckOnce = 0
+
+			if tt.wantPanic {
+				require.Panics(t, func() { checkVersionFn() })
+			} else {
+				require.NotPanics(t, func() { checkVersionFn() })
+			}
+		})
+	}
+}
+
+func TestVersion_Entrypoints(t *testing.T) {
+	tests := []struct {
+		name       string
+		entrypoint func()
+	}{
+		{
+			name: "OpenContext",
+			entrypoint: func() {
+				ctx, err := OpenContext(ContextConfig{})
+				require.NoError(t, err)
+				defer ctx.Close()
+			},
+		},
+		{
+			name: "OpenSender",
+			entrypoint: func() {
+				ctx, err := OpenContext(ContextConfig{})
+				require.NoError(t, err)
+				defer ctx.Close()
+
+				sender, err := OpenSender(ctx, makeSenderConfig())
+				require.NoError(t, err)
+				defer sender.Close()
+			},
+		},
+		{
+			name: "OpenReceiver",
+			entrypoint: func() {
+				ctx, err := OpenContext(ContextConfig{})
+				require.NoError(t, err)
+				defer ctx.Close()
+
+				recv, err := OpenReceiver(ctx, makeReceiverConfig())
+				require.NoError(t, err)
+				defer recv.Close()
+			},
+		},
+		{
+			name: "ParseEndpoint",
+			entrypoint: func() {
+				_, err := ParseEndpoint("rtp://192.168.0.1:1234")
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "SetLogLevel",
+			entrypoint: func() {
+				SetLogLevel(defaultLogLevel)
+			},
+		},
+		{
+			name: "SetLogger",
+			entrypoint: func() {
+				SetLogger(nil)
+			},
+		},
+		{
+			name: "SetLoggerFunc",
+			entrypoint: func() {
+				SetLoggerFunc(nil)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			originalCheckVersionFn := checkVersionFn
+			defer func() { checkVersionFn = originalCheckVersionFn }()
+
+			versionCheckCalled := false
+			checkVersionFn = func() { versionCheckCalled = true }
+
+			tt.entrypoint()
+			require.True(t, versionCheckCalled)
+		})
+	}
 }
 
 func TestVersion_Validite(t *testing.T) {
@@ -93,148 +208,66 @@ func TestVersion_Validite(t *testing.T) {
 	}
 }
 
-func TestVersion_Entrypoints(t *testing.T) {
-	tests := []struct {
-		name       string
-		entrypoint func()
-	}{
-		{
-			name: "OpenContext",
-			entrypoint: func() {
-				ctx, err := OpenContext(ContextConfig{})
-				require.NoError(t, err)
-				defer ctx.Close()
-			},
-		},
-		{
-			name: "OpenSender",
-			entrypoint: func() {
-				ctx, err := OpenContext(ContextConfig{})
-				require.NoError(t, err)
-				defer ctx.Close()
-
-				sender, err := OpenSender(ctx, makeSenderConfig())
-				require.NoError(t, err)
-				defer sender.Close()
-			},
-		},
-		{
-			name: "OpenReceiver",
-			entrypoint: func() {
-				ctx, err := OpenContext(ContextConfig{})
-				require.NoError(t, err)
-				defer ctx.Close()
-
-				recv, err := OpenReceiver(ctx, makeReceiverConfig())
-				require.NoError(t, err)
-				defer recv.Close()
-			},
-		},
-		{
-			name: "ParseEndpoint",
-			entrypoint: func() {
-				_, err := ParseEndpoint("rtp://192.168.0.1:1234")
-				require.NoError(t, err)
-			},
-		},
-		{
-			name: "SetLogLevel",
-			entrypoint: func() {
-				SetLogLevel(defaultLogLevel)
-			},
-		},
-		{
-			name: "SetLogger",
-			entrypoint: func() {
-				SetLogger(nil)
-			},
-		},
-		{
-			name: "SetLoggerFunc",
-			entrypoint: func() {
-				SetLoggerFunc(nil)
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			versionCheckCalled := false
-			versionCheckFn = func() {
-				versionCheckCalled = true
-			}
-			defer func() {
-				versionCheckFn = versionCheck
-			}()
-
-			tt.entrypoint()
-			require.True(t, versionCheckCalled)
-		})
-	}
-}
-
 func TestVersion_Parse(t *testing.T) {
-	type args struct {
-		s string
-	}
 	tests := []struct {
 		name      string
-		args      args
+		arg       string
 		want      SemanticVersion
 		wantPanic bool
 	}{
 		{
 			name: "valid",
-			args: args{
-				s: "3.2.1",
-			},
-			want: SemanticVersion{
-				Major: 3,
-				Minor: 2,
-				Patch: 1,
-			},
+			arg:  "3.2.1",
+			want: SemanticVersion{3, 2, 1},
 		},
 		{
-			name: "doesn't have 3 parts",
-			args: args{
-				s: "3.2",
-			},
+			name:      "empty",
+			arg:       "",
 			wantPanic: true,
 		},
 		{
-			name: "major invalid",
-			args: args{
-				s: "a.2.1",
-			},
+			name:      "garbage",
+			arg:       "xxx",
 			wantPanic: true,
 		},
 		{
-			name: "minor invalid",
-			args: args{
-				s: "3.a.1",
-			},
+			name:      "less parts",
+			arg:       "3.2",
 			wantPanic: true,
 		},
 		{
-			name: "patch invalid",
-			args: args{
-				s: "3.2.a",
-			},
+			name:      "more parts",
+			arg:       "3.2.1.0",
+			wantPanic: true,
+		},
+		{
+			name:      "major invalid",
+			arg:       "a.2.1",
+			wantPanic: true,
+		},
+		{
+			name:      "minor invalid",
+			arg:       "3.a.1",
+			wantPanic: true,
+		},
+		{
+			name:      "patch invalid",
+			arg:       "3.2.a",
 			wantPanic: true,
 		},
 		{
 			name: "hard-coded bindings version",
-			args: args{
-				s: bindingsVersion,
-			},
+			arg:  bindingsVersion,
+			want: parseVersion(bindingsVersion),
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if tt.wantPanic {
-				require.Panics(t, func() { parseVersion(tt.args.s) })
-			} else if tt.want != (SemanticVersion{}) {
-				require.Equal(t, tt.want, parseVersion(tt.args.s))
+				require.Panics(t, func() { parseVersion(tt.arg) })
+			} else {
+				require.Equal(t, tt.want, parseVersion(tt.arg))
 			}
 		})
 	}
