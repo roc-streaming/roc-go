0df41a55c86ac72b10cd3b8039b7c5517d073538
Victor Gaydov <victor@enise.org>
2023-05-31 16:02:38 +0400

    Release 0.2.1

--- roc/version.go
+++ roc/version.go
@@ -17,7 +17,7 @@ import (
 // Hard-coded version of bindings.
 // Should be updated manually each time a new release is tagged.
 // This variable is modified only in tests.
-var bindingsVersion = "0.2.0"
+var bindingsVersion = "0.2.1"
 
 // Validate version compatibility of Go bindings and native library.
 // Must be invoked at all library entry points at least once.
