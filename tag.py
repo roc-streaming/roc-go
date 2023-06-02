c04860cddc5d68c28b3405db61d73bf10003a6fe
abushmin <diskbu@yandex.ru>
2023-06-02 18:57:46 +0400

    review fixes

--- tag.py
+++ tag.py
@@ -3,9 +3,11 @@ import argparse
 import os
 import re
 import subprocess
+import fileinput
+import sys
 
 def check_version_format(version):
-    pattern = re.compile(r'^\d+\.\d+\.\d+$')
+    pattern = re.compile(r'^v?\d+\.\d+\.\d+$')
     return pattern.match(version) is not None
 
 def check_tag_exists(tag):
@@ -16,43 +18,47 @@ def check_tag_exists(tag):
         return False
 
 def write_version(version):
-    with open('roc/version.go', 'r') as f:
-        lines = f.readlines()
+    print(f'Updating version in roc/version.go to {version}')
+    version_line = re.compile(r'\bbindingsVersion\s*=\s*".*"')
+    with fileinput.FileInput('roc/version.go', inplace=True) as f:
+        for line in f:
+            print(version_line.sub(f'bindingsVersion = "{version}"', line), end='')
 
-    with open('roc/version.go', 'w') as f:
-        for line in lines:
-            if line.startswith('var bindingsVersion ='):
-                line = f'var bindingsVersion = "{version}"\n'
-            f.write(line)
+def run_command(args):
+    print(f'Running command: {" ".join(args)}')
+    subprocess.check_call(args)
 
 def commit_change(version):
-    subprocess.check_call(['git', 'add', 'roc/version.go'])
-    subprocess.check_call(['git', 'commit', '-m', f'Release {version}'])
+    run_command(['git', 'add', 'roc/version.go'])
+    run_command(['git', 'commit', '-m', f'Release {version}'])
 
 def create_tag(tag):
-    subprocess.check_call(['git', 'tag', tag])
+    run_command(['git', 'tag', tag])
 
 def push_change(remote, tag):
-    subprocess.check_call(['git', 'push', remote, 'HEAD'])
-    subprocess.check_call(['git', 'push', remote, tag])
+    run_command(['git', 'push', remote, 'HEAD'])
+    run_command(['git', 'push', remote, tag])
 
 def main():
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--push', required=False, help='remote to push')
     parser.add_argument('version', help='version to release')
     args = parser.parse_args()
 
     version = args.version
+    if not check_version_format(version):
+        print(f'Error: version "{version}" is not in correct format. Correct format is "x.y.z" or "vx.y.z"', file=sys.stderr)
+        sys.exit(1)
+
+    version = version.lstrip("v")
     remote = args.push
     tag = f'v{version}'
 
-    if not check_version_format(version):
-        print(f'Error: version "{version}" is not in correct format. Correct format is "x.y.z"')
-        return
-
     if check_tag_exists(tag):
-        print(f'Error: tag "{tag}" already exists')
-        return
+        print(f'Error: tag "{tag}" already exists', file=sys.stderr)
+        sys.exit(1)
 
     write_version(version)
     commit_change(version)
