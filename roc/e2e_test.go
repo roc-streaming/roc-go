e7c2cfc371d0c12b735da4ae758034070141f6bc
abushmin <diskbu@yandex.ru>
2023-07-02 16:09:35 +0400

    improve e2e test

--- roc/e2e_test.go
+++ roc/e2e_test.go
@@ -1,9 +1,12 @@
 package roc
 
 import (
+	"math"
 	"sync"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -82,48 +85,78 @@ func TestEnd2End_Default(t *testing.T) {
 	e := newE2E(t)
 	defer e.close(t)
 
-	samplesCnt := 2
+	samplesCnt := 100
 	samples := make([]float32, samplesCnt)
-	for i := 0; i < samplesCnt; i++ {
-		samples[i] = float32(i + 1)
+	for i := 0; i < samplesCnt/2; i++ {
+		samples[i*2] = float32(i+1) / 100
+		samples[i*2+1] = -float32(i+1) / 100
 	}
+
+	var interval = time.Second / time.Duration(44100/samplesCnt)
+	sendTicker := time.NewTicker(interval)
+	defer sendTicker.Stop()
+
+	receiveTicker := time.NewTicker(interval)
+	defer receiveTicker.Stop()
 
 	endChan := make(chan struct{})
 	var wait sync.WaitGroup
 	wait.Add(1)
 	go func() {
 		for {
-			err := e.Sender.WriteFloats(samples)
-			if err != nil {
-				t.Fail()
-			}
 			select {
+			case <-sendTicker.C:
+				err := e.Sender.WriteFloats(samples)
+				if err != nil {
+					t.Fail()
+				}
 			case <-endChan:
 				wait.Done()
 				return
-			default:
 			}
 		}
 	}()
 
-	for {
-		recFloats := make([]float32, samplesCnt)
-		err := e.Receiver.ReadFloats(recFloats)
-		if err != nil {
-			t.Fail()
-		}
-
-		nonZeroSamplesCnt := 0
-		for i := 0; i < samplesCnt; i++ {
-			if recFloats[i] != 0 {
-				nonZeroSamplesCnt++
+	streamingStarted := false
+	resultSamples := make([]float32, 0, 10000)
+	recFloats := make([]float32, samplesCnt)
+	for len(resultSamples) < cap(resultSamples) {
+		select {
+		case <-receiveTicker.C:
+			err := e.Receiver.ReadFloats(recFloats)
+			if err != nil {
+				t.Fail()
 			}
-		}
-		if nonZeroSamplesCnt == samplesCnt {
-			break
+			for _, v := range recFloats {
+				if !streamingStarted && (v != 0) {
+					streamingStarted = true
+				}
+				if streamingStarted {
+					resultSamples = append(resultSamples, v)
+				}
+			}
 		}
 	}
 
 	endChan <- struct{}{}
 	wait.Wait()
+
+	prevL := resultSamples[0]
+	prevR := resultSamples[1]
+
+	for i := 1; i < len(resultSamples)/2; i++ {
+		valueL := resultSamples[i*2]
+		valueR := resultSamples[i*2+1]
+		if valueL == 0 { // packet loss
+			assert.Equal(t, 0, valueR)
+		} else {
+			assert.Equal(t, valueL, -valueR)
+			if math.Abs(float64(prevL-0.5)) >= 0.0001 {
+				assert.InDelta(t, valueL, prevL+0.01, 0.0001)
+				assert.InDelta(t, valueR, prevR-0.01, 0.0001)
+			}
+		}
+		prevL = valueL
+		prevR = valueR
+	}
 }
