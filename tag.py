0c41995a0143a118b44ded00574a7a9a6c11bf16
Geoffrey <44409380+pypaut@users.noreply.github.com>
2023-09-15 11:04:56 +0200

    Add --force option to tag script (#108) (#114)
    Co-authored-by: Geoffrey JOUNT <gjount@scaleway.com>

--- tag.py
+++ tag.py
@@ -28,16 +28,30 @@ def run_command(args):
     print(f'--- Running command: {" ".join(args)}')
     subprocess.check_call(args)
 
+def check_version_to_commit():
+    cmd = ['git', 'diff', '--cached', 'roc/version.go']
+    return subprocess.check_output(cmd) != b''
+
 def commit_change(version):
     run_command(['git', 'add', 'roc/version.go'])
+    if not check_version_to_commit():
+        print('--- Version did not change, nothing to commit')
+        return
     run_command(['git', 'commit', '-m', f'Release {version}'])
 
-def create_tag(tag):
-    run_command(['git', 'tag', tag])
+def create_tag(tag, force):
+    if force:
+        run_command(['git', 'tag', '-f', tag])
+    else:
+        run_command(['git', 'tag', tag])
 
-def push_change(remote, tag):
+
+def push_change(remote, tag, force):
     run_command(['git', 'push', remote, 'HEAD'])
-    run_command(['git', 'push', remote, tag])
+    if force:
+        run_command(['git', 'push', '-f', remote, tag])
+    else:
+        run_command(['git', 'push', remote, tag])
 
 def main():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -45,6 +59,8 @@ def main():
     parser = argparse.ArgumentParser(description='Create and push tag for new release')
     parser.add_argument('--push', metavar='REMOTE', required=False,
                         help='remote to push (e.g. "origin")')
+    parser.add_argument('-f', '--force', action='store_true', required=False,
+                        help='force creation of tag, even when already exists')
     parser.add_argument('version', metavar='VERSION',
                         help='version to release ("N.N.N" or "vN.N.N")')
     args = parser.parse_args()
@@ -57,17 +73,18 @@ def main():
 
     version = version.lstrip('v')
     remote = args.push
+    force = args.force
     tag = f'v{version}'
 
-    if check_tag_exists(tag):
+    if not force and check_tag_exists(tag):
         print(f'Error: tag "{tag}" already exists', file=sys.stderr)
         sys.exit(1)
 
     write_version(version)
-    commit_change(version)
-    create_tag(tag)
+    commit_change(version, force)
+    create_tag(tag, force)
     if remote:
-        push_change(remote, tag)
+        push_change(remote, tag, force)
     print(f'--- Successfully released {tag}')
 
 if __name__ == '__main__':
