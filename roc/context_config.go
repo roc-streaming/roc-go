e33680fe8000dfc9cab95ef2e732ccb1e85dddc4
Victor Gaydov <victor@enise.org>
2024-06-05 23:56:31 +0400

    Update to 0.2.6

--- roc/context_config.go
+++ roc/context_config.go
@@ -5,14 +5,16 @@ package roc
 // See also Context.
 type ContextConfig struct {
 	// Maximum size in bytes of a network packet.
-	// Defines the amount of bytes allocated per network packet.
-	// Sender and receiver won't handle packets larger than this.
-	// If zero, default value is used.
+	//
+	// Defines the amount of bytes allocated per network packet. Sender and
+	// receiver won't handle packets larger than this. If zero, default value is
+	// used.
 	MaxPacketSize uint32
 
 	// Maximum size in bytes of an audio frame.
+	//
 	// Defines the amount of bytes allocated per intermediate internal frame in the
-	// pipeline. Does not limit the size of the frames provided by user.
-	// If zero, default value is used.
+	// pipeline. Does not limit the size of the frames provided by user. If zero,
+	// default value is used.
 	MaxFrameSize uint32
 }
--- roc/receiver_config.go
+++ roc/receiver_config.go
@@ -7,71 +7,79 @@ import "time"
 // See also Receiver.
 type ReceiverConfig struct {
 	// The rate of the samples in the frames returned to the user.
-	// Number of samples per channel per second.
-	// Should be set.
+	//
+	// Number of samples per channel per second. Should be set.
 	FrameSampleRate uint32
 
 	// The channel set in the frames returned to the user.
+	//
 	// Should be set.
 	FrameChannels ChannelSet
 
 	// The sample encoding in the frames returned to the user.
+	//
 	// Should be set.
 	FrameEncoding FrameEncoding
 
 	// Clock source to use.
-	// Defines whether read operation will be blocking or non-blocking.
-	// If zero, default value is used.
+	//
+	// Defines whether read operation will be blocking or non-blocking. If zero,
+	// default value is used.
 	ClockSource ClockSource
 
 	// Resampler backend to use.
 	ResamplerBackend ResamplerBackend
 
 	// Resampler profile to use.
+	//
 	// If non-zero, the receiver employs resampler for two purposes:
 	//  - adjust the sender clock to the receiver clock, which may differ a bit
-	//  - convert the packet sample rate to the frame sample rate if they are different
+	//  - convert the packet sample rate to the frame sample rate if they are
+	//    different
 	ResamplerProfile ResamplerProfile
 
 	// Target latency, in nanoseconds.
-	// The session will not start playing until it accumulates the requested latency.
-	// Then, if resampler is enabled, the session will adjust its clock to keep actual
-	// latency as close as possible to the target latency.
-	// If zero, default value is used. Should not be negative, otherwise an error is returned.
+	//
+	// The session will not start playing until it accumulates the requested
+	// latency. Then, if resampler is enabled, the session will adjust its clock to
+	// keep actual latency as close as possible to the target latency. If zero,
+	// default value is used.
 	TargetLatency time.Duration
 
 	// Maximum delta between current and target latency, in nanoseconds.
-	// If current latency becomes larger than the target latency plus this value, the
-	// session is terminated.
-	// If zero, default value is used. Should not be negative, otherwise an error is returned.
+	//
+	// If current latency becomes larger than the target latency plus this value,
+	// the session is terminated. If zero, default value is used.
 	MaxLatencyOverrun time.Duration
 
 	// Maximum delta between target and current latency, in nanoseconds.
-	// If current latency becomes smaller than the target latency minus this value, the
-	// session is terminated.
-	// May be larger than the target latency because current latency may be negative,
-	// which means that the playback run ahead of the last packet received from network.
-	// If zero, default value is used.  Should not be negative, otherwise an error is returned.
+	//
+	// If current latency becomes smaller than the target latency minus this value,
+	// the session is terminated. May be larger than the target latency because
+	// current latency may be negative, which means that the playback run ahead of
+	// the last packet received from network. If zero, default value is used.
 	MaxLatencyUnderrun time.Duration
 
 	// Timeout for the lack of playback, in nanoseconds.
-	// If there is no playback during this period, the session is terminated.
-	// This mechanism allows to detect dead, hanging, or broken clients
-	// generating invalid packets.
-	// If zero, default value is used. If negative, the timeout is disabled.
+	//
+	// If there is no playback during this period, the session is terminated. This
+	// mechanism allows to detect dead, hanging, or broken clients generating
+	// invalid packets. If zero, default value is used. If negative, the timeout is
+	// disabled.
 	NoPlaybackTimeout time.Duration
 
 	// Timeout for broken playback, in nanoseconds.
+	//
 	// If there the playback is considered broken during this period, the session
-	// is terminated. The playback is broken if there is a breakage detected at every
-	// BreakageDetectionWindow during BrokenPlaybackTimeout.
-	// This mechanism allows to detect vicious circles like when all client packets
-	// are a bit late and receiver constantly drops them producing unpleasant noise.
-	// If zero, default value is used. If negative, the timeout is disabled.
+	// is terminated. The playback is broken if there is a breakage detected at
+	// every BreakageDetectionWindow during BrokenPlaybackTimeout. This mechanism
+	// allows to detect vicious circles like when all client packets are a bit late
+	// and receiver constantly drops them producing unpleasant noise. If zero,
+	// default value is used. If negative, the timeout is disabled.
 	BrokenPlaybackTimeout time.Duration
 
 	// Breakage detection window, in nanoseconds.
-	// If zero, default value is used. Should not be negative, otherwise an error is returned.
-	// See BrokenPlaybackTimeout.
+	//
+	// If zero, default value is used. See BrokenPlaybackTimeout.
 	BreakageDetectionWindow time.Duration
 }
